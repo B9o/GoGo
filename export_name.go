2999b69765a0e0fe090e9c4600de9e4d6d6eff48
Bingo <faaa@live.com>
2022-02-14 14:45:33 +0800

    init

---
+++ export_name.go
@@ -0,0 +1,11 @@
+// 导出名 https://tour.go-zh.org/basics/3
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Pi)
+}
---
+++ function.go
@@ -0,0 +1,12 @@
+// https://tour.go-zh.org/basics/4
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+}
